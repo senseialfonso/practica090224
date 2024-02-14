ffeab79c07fa7d5c0c296446bdda4b339800a7d9
SenseiAlfonso <alfonsoske@gmail.com>
2024-02-13 20:50:28 -0600

    Agregamos 2 clases nuevas y implementamos nuevos objetos

--- src/main/java/com/mycompany/youtube/Youtube.java
+++ src/main/java/com/mycompany/youtube/Youtube.java
@@ -15,5 +15,11 @@ public class Youtube {
         Video video = new Video("aprendiendo java",2400,"http;//limon.co");
         usuario.subirVideo(video);
         usuario.mostrarInfoUsuario();
+        
+        Usuario usuario2=new Usuario("PERLA");
+        
+       Comentario comment1=new Comentario("Asco de página",usuario2);
+       
+        System.out.println("El usuario "+ usuario2.getNombre()+ " publicó el comentario "+ comment1.getContenido() );
     }
 }
